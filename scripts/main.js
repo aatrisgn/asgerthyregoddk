f7b50d1c78d55db44a1e2d407cd119d1cdd741f3
Asger <asger.thyregod@gmail.com>
2022-10-04 20:17:33 +0200

    Updated work experience and minor bug

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,24 @@ AOS.init({
 
 function DisplayError(){
   var errorMessageElement = document.getElementById("sendErrorMessage");
-  errorMessageElement.innerHTML = "Form is not working. Please send me a regular email at asger.thyregod@gmail.dk.";
+  errorMessageElement.innerHTML = "Form is not working. Please send me a regular email at asger.thyregod@gmail.com.";
 }
+
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
+
+var ageValue = document.getElementById("ageValue");
+ageValue.innerHTML = getAge('1994-04-26');
+
+var ageValue = document.getElementById("profesionalValue");
+ageValue.innerHTML = getAge('2018-08-01');
 
 // Add your javascript here
